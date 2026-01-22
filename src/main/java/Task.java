6941d648fc680c951ddff2bcba3ba58fba66095d
Kacey Isaiah Yonathan <kaceyyonathan@gmail.com>
2026-01-22 23:32:50 +0800

    Added new Task class to replace primitive

---
+++ src/main/java/Task.java
@@ -0,0 +1,22 @@
+public class Task {
+    private boolean marked;
+    private String echo;
+
+    public Task(String echo) {
+        this.marked = false;
+        this.echo = echo;
+    }
+
+    public void mark() {
+        this.marked = true;
+    }
+
+    public voic unmark() {
+        this.marked = false;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + (this.marked ? " " : "X") + "] " + this.echo;
+    }
+}
